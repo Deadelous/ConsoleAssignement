6b69d1acda05fe1f87cc5fb7a863e100b28da596
Leo H <leoh1992@live.nl>
2022-01-16 23:17:13 +0100

    Add Newtonsoft in console application
    - added basic folder structure
    - Created two Entities (Order and Product)
    - WiP in Didata program

---
+++ src/Opdracht/implementatie/Didata/Didata.Core/Entities/OrderEntity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Didata.Core.Entities
+{
+  public class OrderEntity
+  {
+    public OrderEntity()
+    {
+
+    }
+
+    public OrderEntity(long orderId, string description, long customerId, IEnumerable<ProductEntity> products)
+    {
+      OrderId = orderId;
+      Description = description;
+      CustomerId = customerId;
+      Products = products;
+    }
+
+    [Required]
+    public long OrderId { get; private set; }
+
+    [MaxLength(100)]
+    public string Description { get; private set; }
+
+    [Required]
+    [Range(0, int.MaxValue, ErrorMessage = "CustomerId should be greater than 0")]
+    public long CustomerId { get; private set; }
+
+    [Required]
+    [Range(1, int.MaxValue, ErrorMessage = "Product should be greater than or equal to 1")]
+    public IEnumerable<ProductEntity> Products { get; set; }
+
+    public override string ToString()
+    {
+      StringBuilder strBuilder = new StringBuilder();
+      foreach (ProductEntity product in Products)
+      {
+        strBuilder.AppendLine(product.ToString());
+      }
+      return $"OrderId: {OrderId} + Description: {Description} + CustomerId {CustomerId} + Products: {strBuilder}";
+    }
+  }
+}
+
---
+++ src/Opdracht/implementatie/Didata/Didata.Core/Entities/ProductEntity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Didata.Core.Entities
+{
+  public class ProductEntity
+  {
+    public ProductEntity()
+    {
+
+    }
+
+    public ProductEntity(string productId, string description, double amount, double price)
+    {
+      ProductId = productId;
+      Description = description;
+      Amount = amount;
+      Price = price;
+    }
+
+    [Required]
+    [RegularExpression("[A-Za-Z0-9]+")]
+    [MaxLength(50)]
+    public string ProductId { get; private set; }
+
+    [MaxLength(200)]
+    public string Description { get; private set; }
+
+    [RegularExpression(@"^\d+(\.\d{1,2})?$")]
+    public double Amount { get; private set; }
+
+    [RegularExpression(@"^\d+(\.\d{1,2})?$")]
+    public double Price { get; private set; }
+
+    public override string ToString()
+    {
+      return $"ProductId: {ProductId} + Description: {Description} + Amount {Amount} + Price {Price}";
+    }
+  }
+}
+
